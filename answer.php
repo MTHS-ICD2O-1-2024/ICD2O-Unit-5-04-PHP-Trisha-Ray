779181a7f27e23f1a67b4a57177e449f89d30c0e
default <trisha.ray@stu.ocsb.ca>
2025-04-29 21:27:10 +0000

    age pricing done

---
+++ answer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!-- ICS2O-Unit5-04-HTML-MDL -->
+
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta
+    name="description"
+    content="Museum prices for day depending on age, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Trisha Ray" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <title>Museum prices for day depending on age, in PHP</title>
+</head>
+
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <script src="./js/script.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Museum prices for day depending on age, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/museum.jpeg" alt="museum" />
+      </div>
+      <div class="page-content-php">
+        <div id="user-info">
+          <?php
+          $theAge = $_GET['age'];
+          $day = $_GET['days'];
+          if (( $day==3 || $day==6 || ($theAge > 12 && $theAge < 21))) {
+            echo "<p>You are eligible for a student discount.</p>";
+          } else {
+            echo "<p>You must pay full price.</p>";
+          }
+          ?>
+          <div class="page-content-answer">
+            <a href="./index.php">Return</a>
+          </div>
+        </div>
+    </main>
+  </div>
+</body>
+
+</html>
